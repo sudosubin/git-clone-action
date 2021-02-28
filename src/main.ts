4be3b088bdaaaf3609d67ce96b4f3dc658d1382e
sudosubin <sudosubin@gmail.com>
2021-03-01 00:41:03 +0900

    Add default action failure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+import * as core from '@actions/core';
+
 export const run = async () => {
-  console.log('main.ts');
+  try {
+    console.log('main.ts');
+  } catch (error) {
+    core.setFailed(`Action failed for uncaught error: ${error}`);
+  }
 };
 
 run();
